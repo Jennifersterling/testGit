5fb47f6a9fbf51da598b5d40039680460a47101e
Apple <apple@Apples-MacBook-Pro-20.local>
2019-06-17 13:25:43 -0700

    Intial save

---
+++ Contents.swift
@@ -0,0 +1,41 @@
+//
+////print ("Hi Brandi" + " Hi Corey")
+//
+//
+////Idea: design Our code to be able to take anything we wwant to say, and put what we want to say out on the console. Our first function:
+////camelcase: spaces are bad, so when we name somethi g, make it one word.
+////first word not capitalized, everywhere after: capitalized
+////inside(): your parameters: what goes into the funtcion
+//// :--> "of type
+////we made our function here
+//func outputName(userInput: String) {
+//
+//// our function goes here
+//print("\(userInput)")
+//}
+////test our function
+//var test = "It is raining, and I thought Miami would be sunny."
+//outputName(userInput: test)
+//
+//5<3
+//12>7
+//6 != 8
+//7==7
+//
+//var luckyNum = 7
+//luckyNum < 10
+//luckyNum == 7
+
+var dogAge = 5
+if dogAge < 2 {
+    print("you are a puppy!")
+}
+else if dogAge > 12 {
+    print("You are elderly!")
+}
+else {
+    print("You are also awesome!")
+}
+
+
+var favoriteF
